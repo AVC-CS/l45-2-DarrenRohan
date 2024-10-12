b9d80d41fb6e981e7397dc7b9a7edc5d37ea7c24
DarrenRohan <darrenrohan64@gmail.com>
2024-10-11 17:51:48 -0700

    Doesn't work

--- main.py
+++ main.py
@@ -4,11 +4,20 @@ import random
 def main():
     total = 0
     numbers = []
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    i = 0
+    
+    while total <= 100:
+        scheisse = random.randint(0, 100)
+        numbers[i] = 0
+        while numbers[i] != scheisse:
+            numbers[i] += 1
+        print(numbers[i])
+        scheisse = 0
+        total += numbers[i]
+        i += 1
+        if total > 100:
+            total - numbers
+    print(total)
 
     print(f'The random values are {numbers}')
     print(f'The total is {total}')
